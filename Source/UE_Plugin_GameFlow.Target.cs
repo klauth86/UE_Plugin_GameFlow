ae51b277fbf6940c1d7ef0367c2575cb7b8c910e
klauth86 <kehel@yandex.ru>
2023-10-22 14:03:36 +0200

    Fixes

--- Source/UE_Plugin_GameFlow.Target.cs
+++ Source/UE_Plugin_GameFlow.Target.cs
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class UE_Plugin_GameFlowTarget : TargetRules
 {
